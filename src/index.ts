1e8d599fc9ceafbe804d95bbd815f3cfde3aefcb
Satjopg <sat7493@yahoo.co.jp>
2022-03-29 23:22:08 +0900

    fix file/variablename

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const REGION = 'asia-northeast1'
 
 admin.initializeApp()
 
-export const test = functions.region(REGION).https.onRequest((req, res) => {
+export const test = functions.region(REGION).https.onRequest((_req, res) => {
   functions.logger.info("/test request")
   res.send('hello')
 })
