2ebf73a50c01803ae0e04c8d9b519382e12602b5
Satjopg <sat7493@yahoo.co.jp>
2022-03-29 23:26:52 +0900

    fixオプション追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import functions from 'firebase-functions';
-import admin from 'firebase-admin'
+import admin from 'firebase-admin';
 
-const REGION = 'asia-northeast1'
+const REGION = 'asia-northeast1';
 
-admin.initializeApp()
+admin.initializeApp();
 
 export const test = functions.region(REGION).https.onRequest((_req, res) => {
-  functions.logger.info("/test request")
-  res.send('hello')
-})
+  functions.logger.info('/test request');
+  res.send('hello');
+});
